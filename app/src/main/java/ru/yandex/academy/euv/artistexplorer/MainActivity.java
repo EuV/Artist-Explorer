67b559143e7bdb6fafd9f68cd5b3a3cafaefb89c
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-22 00:59:37 +0400

    Add toolbar with UP button

--- app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
+++ app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
@@ -2,8 +2,12 @@ package ru.yandex.academy.euv.artistexplorer;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import ru.yandex.academy.euv.artistexplorer.fragment.ArtistDetailFragment;
 import ru.yandex.academy.euv.artistexplorer.fragment.ArtistListFragment;
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements OnArtistSelectedL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setUpToolbar();
+
         if (savedInstanceState != null) {
             return;
         }
@@ -25,6 +31,44 @@ public class MainActivity extends AppCompatActivity implements OnArtistSelectedL
                 .add(R.id.fragment_container, ArtistListFragment.newInstance())
                 .commit();
     }
+
+
+    /**
+     * Sets up support toolbar itself, its callback and 'UP' button behavior
+     */
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        showUpButtonIfNeeded();
+
+        toolbar.setNavigationOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        getSupportFragmentManager().addOnBackStackChangedListener(new OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                showUpButtonIfNeeded();
+            }
+        });
+    }
+
+
+    /**
+     * Shows or hides toolbar's 'UP' button when there is something on the back stack.
+     * In current implementation, the button is shown when artist details are on the screen
+     * and is hidden when list of artists is displayed.
+     */
+    private void showUpButtonIfNeeded() {
+        boolean showUpButton = (getSupportFragmentManager().getBackStackEntryCount() != 0);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(showUpButton);
+    }
+
 
     @Override
     public void onArtistSelected(@NonNull String artistName) {
