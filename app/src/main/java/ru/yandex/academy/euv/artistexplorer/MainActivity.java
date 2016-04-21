487d7653f5f7fded38bda7facdf5d0668d39ec1b
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-21 22:31:38 +0400

    Let the marathon begin!

---
+++ app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.yandex.academy.euv.artistexplorer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
