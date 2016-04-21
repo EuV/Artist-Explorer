0ef6a75b2d23f440d57daac4dda67af070e68289
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-22 01:45:16 +0400

    Add OkHttp and Fresco to the project

---
+++ app/src/main/java/ru/yandex/academy/euv/artistexplorer/App.java
@@ -0,0 +1,20 @@
+package ru.yandex.academy.euv.artistexplorer;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.imagepipeline.backends.okhttp.OkHttpImagePipelineConfigFactory;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.squareup.okhttp.OkHttpClient;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        ImagePipelineConfig config = OkHttpImagePipelineConfigFactory
+                .newBuilder(this, new OkHttpClient())
+                .build();
+        Fresco.initialize(this, config);
+    }
+}
