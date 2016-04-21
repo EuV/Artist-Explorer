933c5fc8eb1180b387d140619148641f691bee6d
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-21 22:53:43 +0400

    Add ArtistListFragment

--- app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
+++ app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
@@ -1,13 +1,25 @@
 package ru.yandex.academy.euv.artistexplorer;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ru.yandex.academy.euv.artistexplorer.fragment.ArtistListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            return;
+        }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragment_container, ArtistListFragment.newInstance())
+                .commit();
     }
 }
