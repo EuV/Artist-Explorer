017e58f54f815e35afa417f240e722a91b2f9449
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-23 14:01:39 +0400

    Set up artist details layout

--- app/src/main/java/ru/yandex/academy/euv/artistexplorer/fragment/ArtistDetailFragment.java
+++ app/src/main/java/ru/yandex/academy/euv/artistexplorer/fragment/ArtistDetailFragment.java
@@ -1,9 +1,11 @@
 package ru.yandex.academy.euv.artistexplorer.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import android.widget.TextView;
 
 import ru.yandex.academy.euv.artistexplorer.Artist;
 import ru.yandex.academy.euv.artistexplorer.R;
+import ru.yandex.academy.euv.artistexplorer.util.I18n;
+import ru.yandex.academy.euv.artistexplorer.view.SquareDraweeView;
 
 public class ArtistDetailFragment extends Fragment {
     private static final String KEY_ARTIST_NAME = "key_artist_name";
@@ -36,8 +40,27 @@ public class ArtistDetailFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_artist_detail, container, false);
-        ((TextView) view.findViewById(R.id.text_artist_name)).setText(artist.getName());
-        return view;
+        View rootView = inflater.inflate(R.layout.fragment_artist_detail, container, false);
+
+        // Small cover has already been cashed
+        SquareDraweeView coverSmall = (SquareDraweeView) rootView.findViewById(R.id.img_artist_cover_small);
+        coverSmall.setImageURI(Uri.parse(artist.getCover().getSmall()));
+
+        // When loaded, a big cover will substitute a small cover
+        SquareDraweeView coverBig = (SquareDraweeView) rootView.findViewById(R.id.img_artist_cover_big);
+        coverBig.setImageURI(Uri.parse(artist.getCover().getBig()));
+
+        TextView genres = (TextView) rootView.findViewById(R.id.text_artist_genres);
+        genres.setText(TextUtils.join(", ", artist.getGenres()));
+
+        String albums = I18n.albums(artist.getAlbums());
+        String tracks = I18n.tracks(artist.getTracks());
+        TextView albumsAndTracks = (TextView) rootView.findViewById(R.id.text_artist_albums_and_tracks);
+        albumsAndTracks.setText(getString(R.string.sep_by_middot, albums, tracks));
+
+        TextView description = (TextView) rootView.findViewById(R.id.text_artist_description);
+        description.setText(artist.getDescription());
+
+        return rootView;
     }
 }
