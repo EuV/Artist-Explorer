3defe68b0b67c550bf9ccd52ff90cb06dc5284d8
Vasilev Evgenij <vasil.ev.genij@gmail.com>
2016-04-24 23:07:27 +0400

    Add simple UI test

---
+++ app/src/androidTest/java/ru/yandex/academy/euv/artistexplorer/BasicBehaviorTest.java
@@ -0,0 +1,35 @@
+package ru.yandex.academy.euv.artistexplorer;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressBack;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+@RunWith(AndroidJUnit4.class)
+public class BasicBehaviorTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activity = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void openArtistDetailsAndReturn() {
+        onView(withId(R.id.artist_recycler_view))
+                .perform(click());
+
+        onView(withId(R.id.text_artist_description))
+                .check(matches(isDisplayed()))
+                .perform(pressBack());
+
+        onView(withId(R.id.artist_recycler_view))
+                .check(matches(isDisplayed()));
+    }
+}
