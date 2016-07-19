6036d895dccf53ce13eefb8c5d4c02548cad981b
Evgenij Vasilev <vasil.ev.genij@gmail.com>
2016-07-19 13:06:42 +0300

    Add menu

--- app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
+++ app/src/main/java/ru/yandex/academy/euv/artistexplorer/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
@@ -78,6 +80,26 @@ public class MainActivity extends AppCompatActivity implements OnArtistSelectedL
                 .beginTransaction()
                 .add(R.id.fragment_container, ArtistListFragment.newInstance())
                 .commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+                return true;
+            case R.id.feedback:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 
